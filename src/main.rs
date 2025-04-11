d451c8ea243c30910676239db50d1047b532ebf2
notsure55 <blackender5123@mailfence.com>
2025-04-11 16:05:57 -0400

    adding stored addresses so you can edit and modify them like cheat engine, and other memory editors alike..

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ fn main() -> Result<()> {
     //env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([640.0, 400.0]) // wide enough for the drag-drop overlay text
+            .with_inner_size([640.0, 400.0])
             .with_drag_and_drop(true),
         ..Default::default()
     };
     let _ = eframe::run_native(
-        "Native file dialogs and drag-and-drop files",
+        "Rem Engine",
         options,
         Box::new(|_cc| Ok(Box::<window::MyApp>::default())),
     );    
--- src/window.rs
+++ src/window.rs
@@ -9,15 +9,22 @@ pub enum Type {
     Address,
 }
 
-#[derive(Default)]
+#[derive(Debug)]
+struct StoredValue {
+    address: String,
+    value: i32,
+}
+
+#[derive(Default, Debug)]
 pub struct MyApp {
     name: Option<String>,        
-    addresses: BTreeMap<usize, Type>,    
+    addresses: BTreeMap<usize, Type>,
+    saved_addresses: Vec<StoredValue>,
     process: process::Process,    
     guess: String,
     value: String,
     type_of_var: Type,
-    show_types: bool,
+    show_types: bool,    
 }
 
 // TODO: implement grid, to store addresses for further inspection,
@@ -81,35 +88,36 @@ impl MyApp {
         Ok(())
     }
     fn show_address_grid(&mut self, _ctx: &egui::Context, ui: &mut egui::Ui) -> Result<(), Box<dyn std::error::Error>> {
-        egui::ScrollArea::vertical().show(ui, |ui| {
-            egui::Grid::new("Addresses").show(ui, |ui| {                            
-                for (address, value_type) in &mut self.addresses {
+        egui::ScrollArea::vertical().id_salt("scroll_area_2").auto_shrink([false; 2]).show(ui, |ui| {
+            egui::Grid::new("New Addresses").spacing(egui::vec2(10.0, 6.0)).show(ui, |ui| {                        
+                let mut count = 0;
+                for (address, value_type) in self.addresses.clone().iter() {                    
                     match value_type {
                         Type::Integer => {
-                            let value: i32 = self.process.read_mem(*address).unwrap();
-                            if ui.button(format!("{:?}", value_type)).clicked() {
-                                *value_type =  Type::Float;
-                            }
-                            ui.label(format!("Address: 0x{:x} | Value:{}", address,value));
+                            let value: i32 = self.process.read_mem(*address).unwrap();                            
+                            ui.label(format!("Address: 0x{:x} | {:?}:Value:{}", address, value_type, value ));
                             if ui.button("Save").clicked() {
-                                
+                                let stored_value = StoredValue {
+                                    address: address.clone().to_string(),
+                                    value: value,                                    
+                                };
+                                self.saved_addresses.push(stored_value);
+                                self.addresses.remove(&address);
                             }
                             ui.end_row();                                            
                         },
                         Type::Float => {
                             let value: f32 = self.process.read_mem(*address).unwrap();
-                            if ui.button(format!("{:?}", value_type)).clicked() {
-                                *value_type =  Type::Integer;
-                            }
-                            ui.label(format!("Address: 0x{:x} | Value:{}", address,value));
+                            ui.label(format!("Address: 0x{:x} | {:?}:Value:{}", address, value_type, value));
                             ui.end_row();                                            
                         }
                         _ => eprintln!("WRONG TYPE"),
-                    }                                    
-                }
+                    }
+                    if count > 100 { break; }
+                    count += 1;
+                }            
             });                            
-        });
-        
+        });        
         Ok(())
     }
     fn find_process(&mut self, _ctx: &egui::Context, ui: &mut egui::Ui) -> Result<(), Box<dyn std::error::Error>> {
@@ -131,21 +139,51 @@ impl MyApp {
         
         Ok(())
     }
+    fn show_saved_addresses(&mut self, _ctx: &egui::Context, ui: &mut egui::Ui) -> Result<(), Box<dyn std::error::Error>> {
+        egui::ScrollArea::vertical().id_salt("scroll_area_1").auto_shrink([false; 2]).show(ui, |ui| {
+            egui::Grid::new("Saved Addresses").spacing(egui::vec2(10.0, 6.0)).show(ui, |ui| {                
+                for stored in self.saved_addresses.iter_mut() {                                                            
+                    /*if ui.button(format!("{:?}", value_type)).clicked() {
+                     *value_type =  Type::Float;
+                    }*/                    
+                    let value_label = ui.label(format!("Address:{} | Value:{}", stored.address, stored.value));
+                    let response = ui.text_edit_singleline(&mut stored.address)
+                        .labelled_by(value_label.id);
+                    if response.lost_focus() {
+                        stored.value = self.process.read_mem(usize::from_str_radix(&stored.address.clone(), 16).unwrap()).unwrap();
+                    }
+                    ui.end_row();                                                            
+                }
+                
+            });                            
+        });
+        
+        Ok(())
+    }
 }
 
 impl eframe::App for MyApp {    
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Application");            
+            ui.heading("Rem Engine");            
             match self.name {
                 Some(ref _name) =>
                 {
                     let _ = self.build_search_option(ctx, ui);
-                    
-                    if !self.addresses.is_empty() {
-                        let _ = self.show_address_grid(ctx, ui);
-                    }
-                    
+
+                    egui::Grid::new("Black").show(ui, |ui| {
+                        let mut size = ui.spacing().interact_size;
+                        size.x = 200.0;
+                        size.y = 200.0;
+                        ui.allocate_ui_with_layout(size, egui::Layout::left_to_right(egui::Align::Min), |ui| {
+                            if !self.addresses.is_empty() {                            
+                                let _ = self.show_address_grid(ctx, ui);                                
+                            }                            
+                            if !self.saved_addresses.is_empty() {
+                                let _ = self.show_saved_addresses(ctx, ui);
+                            }
+                        });                        
+                    });
                 },
                 None =>
                 {
